8c198c7457ea74ab72592e620bbad7b9895c2371
LuciDevilCoder <lucidevilcoder@gmail.com>
2022-07-16 21:53:23 +0530

    Build cryptocurrencies page details

--- src/index.js
+++ src/index.js
@@ -6,11 +6,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App"
 import "antd/dist/antd.min.css"
 
+import store from "./app/store"
+import { Provider } from "react-redux"
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
     <Router>
-        <App />
+        <Provider store = {store}>
+            <App />
+        </Provider>
     </Router>
 );
 
